7887aa6c97ece387e164a51372d5df0b74b1b1ca
MateoAlarcon <jmalarcon@ucundinamarca.edu.co>
2025-02-27 13:16:16 -0500

    switch case

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+         int materia = 5;
+         String nombreMateria = "x";
+
+         switch(materia){
+             case(1)->nombreMateria="Calculo Integral";
+             case(2)->nombreMateria="Programacion";
+             case(3)->nombreMateria="Ingles";
+             case(4)->nombreMateria="Lectura Critica";
+             case(5)->nombreMateria="Fisica";
+             }
+        System.out.println("tenemos clase de " + nombreMateria);
+        }
+    }
